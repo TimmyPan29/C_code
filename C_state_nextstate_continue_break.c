9953cefe6bb611dac973d1ef6454440ab93add14
TimmyPan29 <q0vm3xu44400@gmail.com>
2023-05-12 03:07:22 +0800

    for loop to while loop

--- C_state_nextstate_continue_break.c
+++ C_state_nextstate_continue_break.c
@@ -6,46 +6,44 @@ int count=0;
 int state=0 ;
 int next_state;
 int enable=0;
-for(int i=0;i<10&&enable==0;i++){ //a.k.a.for(int i=0;i<10,enable==0;i++)
-	if(i<6){
+while(!enable){ //a.k.a.for(int i=0;i<10,enable==0;i++)
+	if(count<6){
 		next_state = 1 ;
 	}
-	else if(i==6) next_state = 2;
+	else if(count==6) next_state = 2;
 	else next_state =3 ;
-switch (state){
-    case 0:
-		printf("state =%d\n",state);
-		printf("next_state =%d\n",next_state);
-		printf("count=%d\n",count);
-		count++;
-		state = next_state ;
+	switch(state){
+		case 0:
+			printf("state =%d\n",state);
+			printf("next_state =%d\n",next_state);
+			printf("count=%d\n",count);
+			count++;
+			state = next_state ;
 
-    case 1:
-		printf("state =%d\n",state);
-		printf("next_state =%d\n",next_state);
-		printf("miaomiaomiao\n");
-		count++;
-		state = next_state ;
-		continue;
+		case 1:
+			printf("state =%d\n",state);
+			printf("next_state =%d\n",next_state);
+			printf("miaomiaomiao\n");
+			count++;
+			state = next_state ;
+			continue;
 
-    case 2:
-		printf("state =%d\n",state);
-		printf("next_state =%d\n",next_state);
-		printf("i=%d\n",i);
-		state = next_state ;
-		count++;
-		continue;
-    case 3:
-		printf("state =%d\n",state);
-		printf("next_state =%d\n",next_state);
-		printf("i=%d\n",i);
-		enable=1;
-      break;//will not process the statement behind "break".and jump out switth
-	//to process "hello\n"
-    default:
-		printf("bye");
-      break;
-} //switch end
+		case 2:
+			printf("state =%d\n",state);
+			printf("next_state =%d\n",next_state);
+			state = next_state ;
+			count++;
+			continue;
+		case 3:
+			printf("state =%d\n",state);
+			printf("next_state =%d\n",next_state);
+			enable=1;
+		  break;//will not process the statement behind "break".and jump out switth
+		//to process "hello\n"
+		default:
+			printf("bye");
+		  break;
+	} //switch end
 	printf("hello\n");
 }//for end
 	printf("\n");
